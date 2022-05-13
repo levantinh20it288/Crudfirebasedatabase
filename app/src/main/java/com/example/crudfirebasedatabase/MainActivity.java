cb3c64feed6fdd476ead00606fea449641cbe4aa
levantinh20it288 <lvtinh.20it6@vku.udn.vn>
2022-05-13 20:27:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/crudfirebasedatabase/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.crudfirebasedatabase;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
